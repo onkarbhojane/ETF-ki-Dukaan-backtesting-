48055625d210b317bed6bd6d996033dcf8ec5d7f
Onkar Bhojane <122461938+onkarbhojane@users.noreply.github.com>
2026-07-15 11:19:42 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,10 +9,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "*",
+    methods: "*",
+    allowedHeaders: "*",
   })
 );
 
